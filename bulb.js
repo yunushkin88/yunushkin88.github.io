57c4acece9241619bfefd6d01ccdffeee659da5c
Dmitry Korzhavin <yunushkin88@mail.ru>
2017-01-23 15:58:47 +0300

    cc1

--- bulb.js
+++ bulb.js
@@ -15,7 +15,7 @@ function connect() {
     console.log('Requesting Bluetooth Device...');
     navigator.bluetooth.requestDevice(
         {
-            filters: [{ services: [0xffe5] }]
+           // filters: [{ services: [0xffe5] }]
         })
         .then(device => {
             console.log('> Found ' + device.name);
