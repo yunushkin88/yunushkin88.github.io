0037379105b2d139f9c320a01d12b8681df28232
Dmitry Korzhavin <yunushkin88@mail.ru>
2017-01-23 17:15:45 +0300

    cccc1

--- bulb.js
+++ bulb.js
@@ -15,7 +15,7 @@ function connect() {
     console.log('Requesting Bluetooth Device...');
     navigator.bluetooth.requestDevice(
         {
-            filters: [{ services: [0xFFFF] }]
+            filters: [{ services: [0xF000] }]
         })
         .then(device => {
             console.log('> Found ' + device.name);
@@ -24,7 +24,7 @@ function connect() {
         })
         .then(server => {
             console.log('Getting Service 0xffe5 - Light control...');
-            return server.getPrimaryService(0xFFFF);
+            return server.getPrimaryService(0xF000);
         })
         .then(service => {
             console.log('Getting Characteristic 0xffe9 - Light control...');
